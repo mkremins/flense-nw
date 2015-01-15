21270b4c4f53074e8d8d39bd1c4baa3c6d8c06ee
Max Kreminski <maxkreminski@gmail.com>
2015-01-15 09:45:57 -0800

    Bump dependency versions

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
 (defproject mkremins/flense-nw "0.0-SNAPSHOT"
   :dependencies
   [[org.clojure/clojure "1.6.0"]
-   [org.clojure/clojurescript "0.0-2496"]
+   [org.clojure/clojurescript "0.0-2665"]
    [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-   [com.facebook/react "0.11.2"]
-   [om "0.7.3"]
+   [com.facebook/react "0.12.2.1"]
+   [org.om/om "0.8.0"]
    [spellhouse/phalanges "0.1.4"]
    [mkremins/flense "0.0-SNAPSHOT"]
    [mkremins/fs "0.3.0"]]
 
   :plugins
-  [[lein-cljsbuild "1.0.3"]]
+  [[lein-cljsbuild "1.0.4"]]
 
   :source-paths ["src"]
 

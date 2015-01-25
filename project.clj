330ffd6fcec2e7f87bda248e6320148546c2f221
Max Kreminski <maxkreminski@gmail.com>
2015-01-25 10:54:49 -0800

    Clean up dependencies and build options
    * Don't depend directly on React (pulled in via Om)
    * Bump ClojureScript to 0.0-2727
    * Bump Om to 0.8.4
    * Drop :preamble (made unnecessary by latest CLJS)
    * Use :optimizations :none to improve compile times

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject mkremins/flense-nw "0.0-SNAPSHOT"
   :dependencies
   [[org.clojure/clojure "1.6.0"]
-   [org.clojure/clojurescript "0.0-2665"]
+   [org.clojure/clojurescript "0.0-2727"]
    [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-   [com.facebook/react "0.12.2.1"]
-   [org.om/om "0.8.0"]
+   [org.omcljs/om "0.8.4"]
    [spellhouse/phalanges "0.1.4"]
    [mkremins/flense "0.0-SNAPSHOT"]
    [mkremins/fs "0.3.0"]]
@@ -12,13 +11,10 @@
   :plugins
   [[lein-cljsbuild "1.0.4"]]
 
-  :source-paths ["src"]
-
   :cljsbuild
   {:builds
    [{:source-paths ["src"]
-     :compiler {:preamble ["react/react.js"]
+     :compiler {:main flense-nw.app
                 :output-to "target/flense.js"
                 :source-map "target/flense.js.map"
-                :optimizations :whitespace
-                :pretty-print true}}]})
+                :optimizations :none}}]})
